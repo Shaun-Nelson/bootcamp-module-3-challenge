4d7fc0fc0abe8a0354a2efad422986ae11c7a24b
Shaun Nelson <shaunptnelson@gmail.com>
2023-03-17 01:02:22 -0400

    Added initial logic to password generator function.

--- script.js
+++ script.js
@@ -1,5 +1,38 @@
 // Assignment code here
+function generatePassword() {
+  let uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  let lowercase = "abcdefghijklmnopqrstuvwxyz";
+  let numeric = "0123456789";
+  let special = " !\"#$%&'()*+,-./:;<=>?@[]^_`{|}~";
+  let characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789 !\"#$%&'()*+,-./:;<=>?@[]^_`{|}~";
 
+  let passwordLength = prompt(
+    "Please enter a password length between 8-128 characters."
+  );
+
+  if (!passwordLength || !(passwordLength >= 8 && passwordLength <= 128)) {
+    alert("Please enter a number between 8-128");
+    generatePassword();
+  }
+
+  let includes = prompt(
+    "Please enter at least one option of characters to include (lowercase, uppercase, numeric, special) seperated by a space."
+  )
+    .toLowerCase()
+    .split(" ");
+
+  if (
+    includes.includes("lowercase") ||
+    includes.includes("uppercase") ||
+    includes.includes("numeric" || includes.includes("special"))
+  ) {
+    console.log("includes", includes);
+  } else {
+    alert("Please select at least one option of characters to include.");
+    generatePassword();
+  }
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -10,7 +43,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
